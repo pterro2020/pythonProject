3e5a540f748ab30b03cd074a694f77bc8e7bf264
pterro2020 <154668100+pterro2020@users.noreply.github.com>
2025-02-11 20:45:25 +0300

    Update wsgi.py

--- pythonProject/wsgi.py
+++ pythonProject/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for pythonProjectPolina project.
+WSGI config for pythonProject project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pythonProjectPolina.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pythonProject.settings')
 
 application = get_wsgi_application()
