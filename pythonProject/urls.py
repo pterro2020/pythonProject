09bf06d038f5c3be108cf1195eae792a81f12d58
pterro2020 <154668100+pterro2020@users.noreply.github.com>
2025-02-11 20:45:02 +0300

    Update urls.py

--- pythonProject/urls.py
+++ pythonProject/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for pythonProjectPolina project.
+URL configuration for pythonProject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
